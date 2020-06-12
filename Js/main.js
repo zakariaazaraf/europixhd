d607b8e7bc7db8bf82b271bf783904d0db0e59e4
zakaria ben salek <zakariaazaraf@gmail.com>
2020-06-12 10:16:25 +0100

    finishing the flip cart property

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+/* let fig = document.querySelector("figure"),
+    card = document.querySelector('.card');;
+
+
+
+card.addEventListener('mouseout', () => {
+    console.log("you pass by me stupied !!");
+    fig.style.transform = "perspective(1000px) rotateY(180deg)"
+}) */
